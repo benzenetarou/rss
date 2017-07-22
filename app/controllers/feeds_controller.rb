df948099c878e41a9dd27da5525abd7999fa992b
benzenetarou <benzenetarou@gmail.com>
2017-07-22 21:00:40 +0900

    add multiple url

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -12,6 +12,18 @@ class FeedsController < ApplicationController
 
   private
     def parse_rss
-      @feed = Feedjira::Feed.fetch_and_parse("http://toyokeizai.net/list/feed/rss")
+      make_array
+      @feed = []
+      @feed_list.each do |url|
+        @feed.push(Feedjira::Feed.fetch_and_parse(url))
+      end
+    end
+
+    def make_array
+      @feed_list = [
+        "http://rssblog.ameba.jp/matsui-kazuyo/rss20.xml",
+        "http://jp.techcrunch.com/feed/",
+        "http://toyokeizai.net/list/feed/rss"
+      ]
     end
 end
