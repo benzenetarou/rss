4e9253a6032637ec46083013a871ec9b8d68c39e
benzenetarou <benzenetarou@gmail.com>
2017-07-23 02:29:26 +0900

    add function to add any rss website

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -3,9 +3,27 @@ class FeedsController < ApplicationController
   def index
     update_database
     @entries = Entry.all.order("published DESC")
+    render 'index'
   end
 
   def new
+    @feed = Feed.new
+  end
+
+  def confirm
+    @url = params[:url]
+    @Feed_to_add = Feedjira::Feed.fetch_and_parse(@url)
+    @feed = Feed.new
+    flash[:success] = "確認します"
+    render 'confirm'
+  end
+
+  def create
+    @feed = Feed.new
+    @feed.title = params[:title]
+    @feed.url = params[:url]
+    @feed.save
+    index
   end
 
   def show
@@ -14,24 +32,20 @@ class FeedsController < ApplicationController
   private
     def parse_rss
       make_array
-      @feed = []
-      @feed_list.each do |url|
-        @feed.push(Feedjira::Feed.fetch_and_parse(url))
+      @rss = []
+      @feed_list.each do |feed|
+        @rss.push(Feedjira::Feed.fetch_and_parse(feed.url))
       end
     end
 
     def make_array
-      @feed_list = [
-        "http://rssblog.ameba.jp/matsui-kazuyo/rss20.xml",
-        "http://jp.techcrunch.com/feed/",
-        "http://toyokeizai.net/list/feed/rss"
-      ]
+      @feed_list = Feed.all
     end
 
     def update_database
         parse_rss
         @new_feed = Feed.new(title: "test", url:"http://jp.techcrunch.com/feed/")
-        @feed.each do |website|
+        @rss.each do |website|
 
           website.entries.each do |entry|
             @new_entry = Entry.new()
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'feeds#index'
-  get 'feeds/new'
-
+  get '/feeds/new', to: 'feeds#new'
+  post 'feeds/confirm', to: 'feeds#confirm'
+  post 'feeds/create', to: 'feeds#create'
   get 'feeds/show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
